63444e22377d26dffdd7e2b68c1984733831ff4c
zhangwenjian <zwj777@live.com>
2022-08-18 12:55:55 +0800

    update

---
+++ precache-manifest.cf9998074fd951440751faa06d7001cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e384b455558480b89614ba92e5947a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a374a043f28ea61a1f",
+    "url": "/static/css/2.fc555424.chunk.css"
+  },
+  {
+    "revision": "2c7707520f23f0ffb2d6",
+    "url": "/static/css/main.3538db62.chunk.css"
+  },
+  {
+    "revision": "82a374a043f28ea61a1f",
+    "url": "/static/js/2.ad011483.chunk.js"
+  },
+  {
+    "revision": "082238424805797633e208386945523e",
+    "url": "/static/js/2.ad011483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7707520f23f0ffb2d6",
+    "url": "/static/js/main.eab0d03c.chunk.js"
+  },
+  {
+    "revision": "4b40b98f5be45c261e1e",
+    "url": "/static/js/runtime-main.f4448e50.js"
+  }
+]);
